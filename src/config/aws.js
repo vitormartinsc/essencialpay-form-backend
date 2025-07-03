f5162d061122bce6e22a1469d517af9a095604eb
essencialsolutions <vitormartinscarvalho@gmail.com>
2025-07-03 17:30:27 -0300

    New commit

--- src/config/aws.js
+++ src/config/aws.js
@@ -1,11 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BUCKET_NAME = exports.s3Client = void 0;
-const client_s3_1 = require("@aws-sdk/client-s3");
-const dotenv_1 = __importDefault(require("dotenv"));
+var client_s3_1 = require("@aws-sdk/client-s3");
+var dotenv_1 = require("dotenv");
 dotenv_1.default.config();
 exports.s3Client = new client_s3_1.S3Client({
     region: process.env.AWS_S3_REGION_NAME || 'us-east-2',
